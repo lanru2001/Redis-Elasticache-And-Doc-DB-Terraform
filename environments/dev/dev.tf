ecca7052bdb6f98bac4799d00641aacd04583d72
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2022-06-17 15:44:53 -0700

    Update dev.tf

--- environments/dev/dev.tf
+++ environments/dev/dev.tf
@@ -1,10 +1,14 @@
+# S3 remote state 
+terraform {
 
+  backend "s3" {
+    bucket         = "tf-remote-bkt3"
+    key            = "project/uat/redis"
+    region         = "us-east-1"
+    dynamodb_table = "cw_locking_dynamodb"
 
-
-
-
-
-
+  }
+}
 
 module "Redis_Cluster" {
 
